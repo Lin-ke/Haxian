ca45f8214f92cef2ab0dd66f805acdab379394f3
V3rgilius <57834731+V3rgilius@users.noreply.github.com>
2023-10-14 16:28:27 +0800

    add minio upload

--- api/utils.py
+++ api/utils.py
@@ -1,6 +1,19 @@
 from typing import List
 from conduit.models import User,Post,Item,Reply
 import json
+from minio import Minio
+from hashlib import sha256
+import io
+minio_conf = {
+    'endpoint': '0.0.0.0:9000',
+    'access_key': 'admin',
+    'secret_key': '123456',
+    'secure': False
+}
+REPLY_PICS_BKT = ""
+POST_PICS_BKT = ""
+IMAGE_MIME = "image/jpeg"
+
 
 def posts_dict(posts: List[Post])->dict:
     ret = []
@@ -26,3 +39,12 @@ def replies_dict(replies: List[Reply])->dict:
 def reply_dict(reply: Reply)->dict:
     temp = reply.__dict__
     return temp
+
+def upload_to_minio(obj:bytes,bucket:str) -> str:
+    client = Minio(**minio_conf)
+    name = sha256(obj).hexdigest()
+    obj_stream = io.BytesIO(obj)
+    client.put_object(bucket_name=bucket, object_name=name,
+                       data=obj_stream,
+                       content_type=IMAGE_MIME)
+    return name
